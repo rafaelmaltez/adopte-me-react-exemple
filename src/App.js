4c29bf158791729a97a2d3de1b7db8c52259a217
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-10-05 20:22:47 -0300

    add SerachParam component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pingo" animal="dog" breed="Mini Golden" />
-      <Pet name="Kit Cat" animal="cat" breed="White Cat" />
+      <SearchParams />
     </div>
   );
 };
