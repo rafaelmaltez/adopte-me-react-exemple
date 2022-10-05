0a083bbbb66f0282ee1e0f0de5f3ae9036883c87
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-10-05 18:18:03 -0300

    add Pet component

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
+const Pet = () => {
+  return (
+    React.createElement("div", {}, [
+      React.createElement("h1", {}, "Luna"),
+      React.createElement("h2", {}, "Dog"),
+      React.createElement("h2", {}, "Havanese")
+    ])
+  )
+}
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
+  return (
+    React.createElement(
+      "div",
+      {},
+      [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet)
+      ]
+    )
   )
 }
 
