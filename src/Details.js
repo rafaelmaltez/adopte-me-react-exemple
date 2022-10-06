06a923547671d705f5314d5431d5f4960dc1c1ae
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-10-06 17:57:05 -0300

    turn Details into a class component

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,37 @@
-import { useParams } from "react-router-dom";
+import { Component } from "react";
+class Details extends Component {
+  constructor(props) {
+    super(props);
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>{id} id</h2>;
-};
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.devs-api.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {city}, {state}{" "}
+        </h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    );
+  }
+}
 
 export default Details;
