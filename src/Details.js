f352183a2e609b2cf7a879f4051180efeebc427d
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-10-06 18:33:52 -0300

    create Carousel component

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,9 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -21,9 +18,11 @@ class Details extends Component {
       return <h2>loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
     return (
       <div className="details">
+        <Carousel images={images} />
         <h1>{name}</h1>
         <h2>
           {animal} - {breed} - {city}, {state}{" "}
